ea30183ce9841386499c860bdaade5fedd2a2c73
marest94 <stojanovic1994@yahoo.com>
2018-03-06 12:04:11 +0100

    First commit

---
+++ src/main/java/com/spring/configs/WebConfig.java
@@ -0,0 +1,22 @@
+package com.spring.configs;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { MvcConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+	
+}
